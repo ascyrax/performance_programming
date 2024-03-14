a1259a3e91b37df37f0aaeec02ea0fed690ccfff
ascyrax <ascyrax01@gmail.com>
2024-03-14 20:59:01 +0530

    added basic profiling to my json parse :)

--- 02 basic profiling/haversine_inputJson_generator.cpp
+++ 02 basic profiling/haversine_inputJson_generator.cpp
@@ -3,20 +3,22 @@
 #include <iostream>
 #include <cstdlib>
 
+#include "haversine_algo.cpp"
+
 void appendToJsonFile(std::ofstream &outfile, double &x0, double &y0, double &x1, double &y1)
 {
     outfile << "{";
-    outfile << "\"x0\":" << x0 << ",";
+    outfile << std::fixed << std::setprecision(16) << "\"x0\":" << x0 << ",";
     // fprintf(outfile, "\"x0\":%.16f", x0);
-    outfile << "\"y0\":" << y0 << ",";
-    outfile << "\"x1\":" << x1 << ",";
-    outfile << "\"y1\":" << y1;
+    outfile << std::fixed << std::setprecision(16) << "\"y0\":" << y0 << ",";
+    outfile << std::fixed << std::setprecision(16) << "\"x1\":" << x1 << ",";
+    outfile << std::fixed << std::setprecision(16) << "\"y1\":" << y1;
     return;
 }
 
-void generate_harvesine_json_input(int &nCords, int &seed)
+void generate_harvesine_json_input(int &nCords, int &seed, std::vector<double> &results)
 {
-    char *keyName = "pairs";
+    const char *keyName = "pairs";
 
     std::mt19937 rn_gen(seed); // initialized a mersenne random no generator which generates higher quality random nos than rand()
     int rnx_min = -180, rnx_max = 180, rny_min = -90, rny_max = 90;
@@ -24,7 +26,7 @@ void generate_harvesine_json_input(int &nCords, int &seed)
     std::uniform_real_distribution<> distrx(rnx_min, rnx_max);
     std::uniform_real_distribution<> distry(rny_min, rny_max);
 
-    char *fileName = "harvesine_data.json";
+    const char *fileName = "haversine_data.json";
     std::ofstream outfile(fileName);
 
     if (!outfile)
@@ -34,13 +36,15 @@ void generate_harvesine_json_input(int &nCords, int &seed)
     }
     outfile << "{\"" << keyName << "\":[" << std::endl;
 
+    // double sum = 0;
     while (nCords)
     {
         double x0 = distrx(rn_gen);
         double y0 = distry(rn_gen);
         double x1 = distrx(rn_gen);
         double y1 = distry(rn_gen);
-
+        results.push_back(ReferenceHaversine(x0, y0, x1, y1));
+        // printf("%.10d")
         appendToJsonFile(outfile, x0, y0, x1, y1);
         if (nCords == 1)
             outfile << "}";
@@ -55,8 +59,18 @@ int main(int argv, char **argc)
 {
     int nCords = atoi(argc[1]); // no of cordinates
     int seed = atoi(argc[2]);   // seed for the marsenne random no generator
+    std::vector<double> results;
 
-    generate_harvesine_json_input(nCords, seed);
+    generate_harvesine_json_input(nCords, seed, results);
+
+    double average = 0;
+    std::size_t lenResults = results.size();
+    for (int i = 0; i < lenResults; i++)
+        average += results[i] / (lenResults * 1.0);
+    printf("average = %.16f\n", average);
+
+    for (int i = 0; i < lenResults; i++)
+        printf("%.16f\n", results[i]);
 
     return 0;
 }
---
+++ 02 basic profiling/haversine_json_parser.cpp
@@ -0,0 +1,206 @@
+#include <fstream>
+// #include <string>
+#include <iostream>
+#include <vector>
+#include <stack>
+#include "haversine_algo.cpp"
+
+// static inline uint64_t rdtsc() // this is for x86 arch
+// {
+//     unsigned int lo, hi;
+//     __asm__ __volatile__("rdtsc" : "=a"(lo), "=d"(hi));
+//     return ((uint64_t)hi << 32) | lo;
+// }
+
+static inline uint64_t read_system_counter()
+{ // this is for arm64 arch
+    uint64_t val;
+    __asm__ volatile(
+        "mrs %0, cntvct_el0"
+        : "=r"(val));
+    return val;
+}
+
+int main(int argCnt, char **args)
+{
+    uint64_t start = read_system_counter();
+
+    if (argCnt < 2)
+    {
+        printf("./haversine_json_parser [ haversine_data.json ]\n");
+        printf("data.json filename needs to be provided :)\n");
+        return 1;
+    }
+    char *fileName = args[1];
+    uint64_t befFileOpen = read_system_counter();
+
+    std::ifstream fileObj(fileName, std::ios::ate);
+    if (!fileObj)
+    {
+        printf("error: could not open file = %s\n", fileName);
+        return 1;
+    }
+    std::streampos fileLen = fileObj.tellg();
+    printf("fileLen = %lld\n", static_cast<long long>(fileLen));
+
+    fileObj.seekg(0, std::ios::beg);
+
+    std::vector<char> jsonData(fileLen);
+
+    std::stack<char> st;
+    std::vector<std::string> tokens;
+
+    uint64_t befFileRead = read_system_counter();
+    if (fileObj.read(jsonData.data(), fileLen))
+    {
+        std::string token;
+        for (int i = 0; i < fileLen; i++)
+        {
+
+            if (jsonData[i] == '{' || jsonData[i] == '[')
+            {
+                st.push(jsonData[i]);
+            }
+            else if (jsonData[i] == '}' || jsonData[i] == ']')
+            {
+                if (st.empty() || (st.top() != jsonData[i] - 2))
+                {
+                    printf("error: bad format in the input json file: %s \n", fileName);
+                    return 1;
+                }
+                st.pop();
+                if (st.empty())
+                {
+                    printf("json parsing complete for file : %s :)\n", fileName);
+                    break;
+                }
+                else
+                {
+                    // printf("data group no: %d read\n", i);
+                    // parseCordinates();
+                    if (token != "")
+                        tokens.push_back(token);
+                    token = "";
+                }
+            }
+            else if (jsonData[i] == '\"')
+            {
+                if (st.top() == '\"')
+                    st.pop();
+                else
+                    st.push('\"');
+            }
+            else if (jsonData[i] == 'p')
+            {
+                // check if the pairs property exist or not
+                if (!(i + 4 < fileLen))
+                {
+                    printf("error: bad format in the input json file: %s \n", fileName);
+                    return 1;
+                }
+                if (!(jsonData[i + 1] == 'a' && jsonData[i + 2] == 'i' && jsonData[i + 3] == 'r' && jsonData[i + 4] == 's'))
+                {
+                    printf("error: property PAIRS not found in the input json file : %s\n", fileName);
+                    return 1;
+                }
+                else
+                {
+                    printf("PAIRS property found :)\n");
+                    i = i + 4;
+                }
+            }
+            else if (jsonData[i] == ':')
+            {
+                // add token to tokens
+                if (token != "")
+                    tokens.push_back(token);
+                token = "";
+            }
+            else if (jsonData[i] == ',')
+            {
+                // add current
+                if (token.size() && isnumber(token.back()))
+                {
+                    if (token != "")
+                        tokens.push_back(token);
+                    token = "";
+                }
+            }
+            else if (isalnum(jsonData[i]) || jsonData[i] == '-' || jsonData[i] == '.')
+            {
+                if (jsonData[i] == ' ')
+                    continue;
+                token += jsonData[i];
+            }
+        }
+        // std::cout << "tokens: \n";
+        // for (std::string token : tokens)
+        //     std::cout << token << std::endl;
+    }
+    uint64_t afterFileRead = read_system_counter();
+
+    fileObj.close();
+    uint64_t afterFileClose = read_system_counter();
+
+    std::vector<double> results;
+    double x0, y0, x1, y1;
+    // for (int i = 0; i < tokens.size(); i++)
+    // {
+    //     printf("i = %d, token[%d] =", i, i);
+    //     std::cout << tokens[i] << std::endl;
+    // }
+    for (int i = 0; i < tokens.size(); i += 8)
+    {
+        for (int j = i; j < i + 8; j += 2)
+        {
+            if (tokens[j] == "x0")
+            {
+                x0 = std::stod(tokens[j + 1]);
+            }
+            else if (tokens[j] == "y0")
+            {
+                y0 = std::stod(tokens[j + 1]);
+            }
+            else if (tokens[j] == "x1")
+            {
+                x1 = std::stod(tokens[j + 1]);
+            }
+            else if (tokens[j] == "y1")
+            {
+                y1 = std::stod(tokens[j + 1]);
+            }
+        }
+        // std::cout << std::fixed << std::setprecision(16) << "x0: " << x0 << ", y0 : " << y0 << ", x1 : " << x1 << ", y1 : " << y1 << std::endl;
+        results.push_back(ReferenceHaversine(x0, y0, x1, y1));
+    }
+    uint64_t afterCalculation = read_system_counter();
+
+    std::size_t lenResults = results.size();
+    double average = 0;
+    for (int i = 0; i < lenResults; i++)
+        printf("%.16f\n", results[i]);
+
+    for (int i = 0; i < lenResults; i++)
+        average += results[i] / (lenResults * 1.0);
+    printf("average = %.16f\n", average);
+
+    uint64_t end = read_system_counter();
+    std::cout << std::endl
+              << "start: " << start << std::endl;
+    std::cout << (befFileOpen - start) << std::endl;
+    std::cout << "befFileOpen: " << befFileOpen << std::endl;
+    std::cout << (befFileRead - befFileOpen) << std::endl;
+    std::cout << "befFileRead: " << befFileRead << std::endl;
+    std::cout << (afterFileRead - befFileRead) << std::endl;
+    std::cout << "afterFileRead: " << afterFileRead << std::endl;
+    std::cout << afterFileClose - afterFileRead << std::endl;
+    std::cout << "afterFileClose: " << afterFileClose << std::endl;
+    std::cout << (afterCalculation - afterFileClose) << std::endl;
+    std::cout << "afterCalculation: " << afterCalculation << std::endl;
+    std::cout << end - afterCalculation << std::endl;
+    std::cout << "end: " << end << std::endl;
+    std::cout << std::endl
+              << "Total Cycles: " << (end - start) << std::endl;
+
+    return 0;
+}
